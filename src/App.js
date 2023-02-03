934be168473276832c689b0f5d82a8bae80a3230
yasvipatel52 <yasvi.patel@marutitech.com>
2023-02-03 14:32:20 +0530

    useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react'
 
 function App() {
+  const [content,setContent]=useState('Hii');
+  const [text,setText]=useState("hello");
+
+  useEffect(()=>{
+     console.log(content) 
+     console.log(text)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <button onClick={()=>{setContent('Posts')}}>Posts</button>
+   <button onClick={()=>{setContent('Comments')}}>Comments</button>
+   <button onClick={()=>{setContent('Images')}}>Images</button>
+   <button onClick={()=>{setText("good morning!!")}}>Text button</button>
+   </>
   );
 }
 
